791605f415670de9192c81ea4635e575dbe9b437
Kai Bepperling <kai.bepperling@gmail.com>
2017-03-16 22:03:37 +0100

    added/implemented batterymeter lib

---
+++ lib/BatteryMeter/src/BatteryMeter.cpp
@@ -0,0 +1,26 @@
+#include "BatteryMeter.h"
+
+int oldBatteryPcnt = 0;
+long oldvoltage = 0;
+int sensorValue = 0;
+int batteryPcnt = 0;
+float voltage = 0;
+
+BatteryMeter::BatteryMeter(int pin)
+{
+	_pin = pin;
+}
+
+float BatteryMeter::checkBatteryLevel()
+{
+  sensorValue = analogRead(_pin);
+	batteryPcnt = sensorValue / 10;
+	voltage = sensorValue * (5.00/1023.00);
+
+	if(oldvoltage != voltage){
+		oldvoltage = voltage;
+		return voltage;
+	}else{
+		return oldvoltage;
+	}
+}
